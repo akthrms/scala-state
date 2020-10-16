abd2203643adcf2b9cd079a178fafd57574e787f
akthrms <akt.hrms@gmail.com>
2020-10-17 02:50:22 +0900

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "scala-state"
+
+version := "0.1"
+
+scalaVersion := "2.13.3"
---
+++ src/main/scala/MainApp.scala
@@ -0,0 +1,23 @@
+object MainApp extends App {}
+
+case class State[S, A](runState: S => (A, S)) {
+  def flatMap[B](fn: A => State[S, B]): State[S, B] =
+    State { s =>
+      val (result, state) = runState(s)
+      fn(result).runState(state)
+    }
+
+  def map[B](fn: A => B): State[S, B] =
+    State { s =>
+      val (result, state) = runState(s)
+      State.pure(fn(result)).runState(state)
+    }
+}
+
+object State {
+  def get[S]: State[S, S] = State { s => (s, s) }
+
+  def put[S](s: S): State[S, Unit] = State { _ => ((), s) }
+
+  def pure[S, A](a: A): State[S, A] = State { s => (a, s) }
+}
