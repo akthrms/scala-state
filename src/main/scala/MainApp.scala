6b1c69f9c0bb3494620841b4dff053bf6f481441
akthrms <akt.hrms@gmail.com>
2020-10-17 11:08:44 +0900

    update

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,6 +1,35 @@
-object MainApp extends App {}
+object MainApp extends App {
+
+  import State._
+
+  type Stack[A] = List[A]
+
+  def pop: State[Stack[Int], Int] =
+    for {
+      stack <- get
+      _ <-
+        if (stack.isEmpty) sys.error("stack is empty.")
+        else put(stack.tail)
+    } yield stack.head
+
+  def push(n: Int): State[Stack[Int], Unit] =
+    for {
+      stack <- get
+      _ <- put(n :: stack)
+    } yield ()
+
+  val stack: State[Stack[Int], Int] =
+    for {
+      _ <- push(4)
+      _ <- push(5)
+      a <- pop
+    } yield a
+
+  assert(stack.runState(List(3, 2, 1)) == (5, List(4, 3, 2, 1)))
+}
 
 case class State[S, A](runState: S => (A, S)) {
+
   def flatMap[B](fn: A => State[S, B]): State[S, B] =
     State { s =>
       val (result, state) = runState(s)
@@ -15,6 +44,7 @@ case class State[S, A](runState: S => (A, S)) {
 }
 
 object State {
+
   def get[S]: State[S, S] = State { s => (s, s) }
 
   def put[S](s: S): State[S, Unit] = State { _ => ((), s) }
